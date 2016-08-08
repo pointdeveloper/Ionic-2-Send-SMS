23f79f5d89e726d2b8fafe6ba62b7bb8415311af
pointdeveloper <pointdeveloper99@gmail.com>
2016-08-08 19:33:53 +0530

    Updated to Ionic 2 TYpescript

---
+++ app/pages/home/home.ts
@@ -0,0 +1,30 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import { SMS } from 'ionic-native';
+
+
+@Component({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  sendSMS(){
+    var options={
+          replaceLineBreaks: false, // true to replace \n by a new line, false by default
+          android: {
+               intent: 'INTENT'  // Opens Default sms app
+              //intent: '' // Sends sms without opening default sms app
+            }
+    }
+    SMS.send('416123456', 'Hello world!',options)
+      .then(()=>{
+        alert("success");
+      },()=>{
+      alert("failed");
+      });
+  }
+  
+}
